621f8d744d4cd984a777d22942bba8b29dfc0e5f
KJAnalytics <106294465+KJAnalytics@users.noreply.github.com>
2022-07-10 21:49:23 -0400

    updated queries.sql

--- Analysis Projects Folder/Pewlett-Hackard-Analysis_Folder/Queries/queries.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis_Folder/Queries/queries.sql
@@ -46,12 +46,19 @@ CREATE TABLE dept_emp (
 	PRIMARY KEY (emp_no, dept_no)
 );
 
+
+--Added Drop Tables Titles.  Initially wouldnt process with Foreign Key.  Dropped and readded after changing position
+ 
+
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
  	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
-  	to_date DATE NOT NULL   
+  	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+ 	PRIMARY KEY (emp_no, title, from_date)
 );
+
 
 --  Code used to confirm imports
 SELECT * FROM departments;    
